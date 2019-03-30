057d3e667bd68feffecfb0ae72d8550241a8226d
mac-00014 <mac-00014@example.com>
2019-03-30 17:17:01 +0530

    Initial Commit

---
+++ RatingView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RatingView
+//
+//  Created by mac-00014 on 30/03/19.
+//  Copyright © 2019 Mind. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
